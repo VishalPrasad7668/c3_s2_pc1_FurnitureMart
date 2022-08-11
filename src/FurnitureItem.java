9499668b6f33c73af5371ae6ca134ec9f8facfb9
VISHAL PRASAD <vishalprasad1117@gmail.com>
2022-08-11 14:50:54 +0530

    behavior or method of the FurnitureItem added

--- src/FurnitureItem.java
+++ src/FurnitureItem.java
@@ -5,4 +5,22 @@ public class FurnitureItem {
     String color;
     String furnitureUsage;
     double price;
+
+    FurnitureItem() {
+        furnitureCode = 0;
+        furnitureType = " ";
+        gradeOfFurniture = " ";
+        color = " ";
+        furnitureUsage = " ";
+        price = 0.0;
+    }
+
+    public double calculateDiscount(double discount) {
+        if (furnitureUsage == "outdoor") {
+            discount = 5;
+            return price + (price * discount / 100);
+        } else {
+            return price;
+        }
+    }
 }
